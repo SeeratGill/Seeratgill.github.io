46b45f31e1263bff0531d8889aee9b1e3a7ae9e9
SeeratGill <gill_harseerat@khalsaschool.ca>
2025-04-15 09:33:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function changeBackgroundColor() {
     document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
   }
   
+  
   function displayMessage(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
